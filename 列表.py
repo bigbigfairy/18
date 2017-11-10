af4a96e82cd962678f5a95096ce8e5ad3937d13a
bigbigfairy <33245360+bigbigfairy@users.noreply.github.com>
2017-11-10 13:59:40 +0800

    Add files via upload

---
+++ 列表.py
@@ -0,0 +1,41 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> list1 = ['physics', 'chemistry', 1997, 2000];
+>>> list2 = [1, 2, 3, 4, 5, 6, 7 ];
+>>> print "list1[0]: ", list1[0]
+list1[0]:  physics
+>>> print "list2[1:5]: ", list2[1:5]
+list2[1:5]:  [2, 3, 4, 5]
+>>> 
+>>> 
+>>> list = ['physics', 'chemistry', 1997, 2000];
+>>> print "Value available at index 2 : "
+Value available at index 2 : 
+>>> print list[2];
+1997
+>>> list[2] = 2001;
+>>> print "New value available at index 2 : "
+New value available at index 2 : 
+>>> print list[2];
+2001
+>>> 
+>>> 
+>>> 
+>>> list1 = ['physics', 'chemistry', 1997, 2000];
+>>> print list1;
+['physics', 'chemistry', 1997, 2000]
+>>> del list1[2];
+>>> print "After deleting value at index 2 : "
+After deleting value at index 2 : 
+>>> print list1;
+['physics', 'chemistry', 2000]
+>>> 
+>>> 
+>>> L = ['Google', 'Runoob', 'Taobao']
+>>> L[2]
+'Taobao'
+>>> L[-2]
+'Runoob'
+>>> L[1:]
+['Runoob', 'Taobao']
+>>> 
